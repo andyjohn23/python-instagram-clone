b024619c60d79f15a412a1eb9060048a78086716
andyjohn23 <andrewnick906@gmail.com>
2021-04-25 10:16:51 +0300

    added the media configurations

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 AUTH_USER_MODEL = "authentication.UserAccount"
 
 # Default primary key field type
--- posts/forms.py
+++ posts/forms.py
@@ -1,4 +1,6 @@
-# from django import forms
-# from posts.models import Post
+from django import forms
+from posts.models import Post
+from cloudinary.models import CloudinaryField
 
-# class NewPostForm(forms.ModelForm):
+class NewPostForm(forms.ModelForm):
+    photo = forms.CloudinaryField
