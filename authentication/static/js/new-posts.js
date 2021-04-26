6bbb801e0d00928eba667d1ff62a5da0db49e73d
andyjohn23 <andrewnick906@gmail.com>
2021-04-26 07:18:31 +0300

    adding javascript validation to new post form

--- authentication/static/js/new-posts.js
+++ authentication/static/js/new-posts.js
@@ -1,9 +1,18 @@
 var img = document.forms["myform"]["image"];
+var validFileEXtension = ["jpeg", "jpg", "png", "gif"];
 
 function validation() {
   if (img.value != "") {
+    var imageDot = img.value.lastIndexOf(".");
+    var imageExtension = img.value.substring(imageDot);
+
+    var result = validFileEXtension.includes(imageExtension);
+    if (result == false) {
+      alert("SELECTED FILE IS NOT AN IMAGE");
+      return false;
+    }
   } else {
-    alert("NO IMAGE SELECTED!!!");
+    alert("NO IMAGE SELECTED");
     return false;
   }
 }
