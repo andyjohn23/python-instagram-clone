e7654a6c058dc30d41c84b779f706fd17496ea59
andyjohn23 <andrewnick906@gmail.com>
2021-05-02 19:34:23 +0300

    updated the post card

--- static/js/posts.js
+++ static/js/posts.js
@@ -1,3 +1,17 @@
-$(document).ready(function () {
-  $("#myTextarea").emojioneArea();
+$(document).on("submit", "#like-form", function (e) {
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: '{% url "postlike" %}',
+    data: {
+      post_id: $("#likes").val(),
+      csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+      action: "post",
+    },
+    success: function (json) {
+      document.getElementById("like_count").innerHTML = json["result"];
+      console.log(json);
+    },
+    error: function (xhr, errmsg, err) {},
+  });
 });
