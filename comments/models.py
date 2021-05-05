932e9d0e09bf947ebad61a79d66f5ced1bd4fb34
andyjohn23 <andrewnick906@gmail.com>
2021-05-05 12:23:55 +0300

    created comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from posts.models import Post
+from authentication.models import UserAccount
+from django.utils import timezone
 
 # Create your models here.
+
+
+class comments(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comment")
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    body = models.TextField()
+    date_commented = models.DateTimeField(default=timezone.now)
