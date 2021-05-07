9c6f739c589fe312729a9f24f9ca30d692fc4ef3
andyjohn23 <andrewnick906@gmail.com>
2021-05-07 15:08:22 +0300

    created the messaging view

--- messaging/views.py
+++ messaging/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Create your views here.
+
+
 @login_required(login_url='index')
 def message(request):
-    return render(request, 'auth/message-box.html')
+    user = request.user
+    messages = Message.get_messages(user=user)
+    active_user = None
+    directs = None
+
+    if messages:
+        message = messages[0]
+        active_user = message['user'].username
+        directs = Message.objects.filter(user=user, recipient=message['user'])
+        directs.update(is_read=True)
+
+        for message in messages:
+            if message['user'].username == active_user:
+                message['unread'] = 0
+
+        context = {
+            'messages': messages,
+            'directs': directs,
+            'active_user': active_user
+        }
+
+    template = loader.get_template('auth/message-box.html')
+
+    return HttpResponse(template.render(context, request))
