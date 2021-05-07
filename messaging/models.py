792f87e412236be445dab71ea6a4b2ef45b75da6
andyjohn23 <andrewnick906@gmail.com>
2021-05-07 15:15:59 +0300

    created the messaging model

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from authentication.models import UserAccount
+from django.utils import timezone
 
 # Create your models here.
+
+class Message(models.Model):
+    user = models.ForeignKey(UserAccount, related_name="user", on_delete=models.CASCADE)
+    sender = models.ForeignKey(UserAccount, related_name="sender", on_delete=models.CASCADE)
+    recipient = models.ForeignKey(UserAccount, related_name="recipient", on_delete=models.CASCADE)
+    body = models.TextField(max_length=5000, blank=True, null=True)
+    date = models.DateTimeField(default=timezone.now)
+    is_read = models.BooleanField(default=False)
+
+    
+
+
